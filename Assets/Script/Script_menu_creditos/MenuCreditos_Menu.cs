c7fb2a52af2267c65f05c3bad4a386aa15aa2f4b
williamsccoa <120693799+williamsccoa@users.noreply.github.com>
2023-01-07 09:08:30 -0500

    menu

---
+++ Assets/Script/Script_menu_creditos/MenuCreditos_Menu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCreditos_Menu : MonoBehaviour
+{
+
+    public void EscenaJuegoCreditos_menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
