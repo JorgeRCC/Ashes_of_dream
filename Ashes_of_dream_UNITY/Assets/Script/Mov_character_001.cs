776381510b1561d842ede5799f361071ce6ddd7f
Brandon Gonzales <brag0nti7pro@gmail.com>
2023-01-10 20:35:29 -0500

    PVG0-37-TrampaConMovimiento
    Cree una Trampa con movimiento que si el jugador hace contacto morirá

--- Ashes_of_dream_UNITY/Assets/Script/Mov_character_001.cs
+++ Ashes_of_dream_UNITY/Assets/Script/Mov_character_001.cs
@@ -1,17 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Mov_character_001 : MonoBehaviour
 {
     public float velocidadMovimiento;
     private Rigidbody2D fisica;
     public float fuerzaSalto = 4.5f;
+
+    int trampasLayer;
     // Start is called before the first frame update
     void Start()
     {
         fisica = GetComponent<Rigidbody2D>();
+
+        trampasLayer = LayerMask.NameToLayer("Trampas");
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == trampasLayer)
+        {
+            //Matar al jugador
+            SceneManager.LoadScene("GameOver");
+            Debug.Log("Muerto");
+        }
+    }
+
+
 
     // Update is called once per frame
     void Update()
